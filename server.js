997ce6c8795fcdf7ea0c17cdb15a7adf05786049
SHUMAIL AHMAD SAMAR <shumailahmad8938@gmail.com>
2026-04-26 22:24:51 +0530

    Added MiddleWare to Server

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 import express from "express";
+import { rateLimiter, initRateLimiter } from "./rateLimiter.js";
+
 const app = express();
 const PORT = 3000;
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+async function start() {
+  await initRateLimiter();
+  app.use(rateLimiter());
+  app.get("/", (req, res) => res.send("Hello World"));
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+start();

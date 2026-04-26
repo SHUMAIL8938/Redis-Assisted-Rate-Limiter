ff25f4a37bc6e9bc2a8a02d2c2b891d90a65a6d8
SHUMAIL AHMAD SAMAR <shumailahmad8938@gmail.com>
2026-04-26 22:23:34 +0530

    Added Lua Script

--- rateLimiter.js
+++ rateLimiter.js
@@ -1,17 +1,35 @@
 import redis from "./redisClient.js";
+const script = `
+local now = tonumber(ARGV[1])
+local window = tonumber(ARGV[2])
+local limit = tonumber(ARGV[3])
+local member = ARGV[4]
+
+redis.call("ZREMRANGEBYSCORE", KEYS[1], 0, now-window)
+local count = redis.call("ZCARD", KEYS[1])
+
+if count >= limit then
+  return {0, count}
+end
+
+redis.call("ZADD", KEYS[1], now, member)
+redis.call("EXPIRE", KEYS[1], math.ceil(window/1000))
+return {1, count+1}
+`;
+
+let sha;
+export async function initRateLimiter() {
+  sha = await redis.script("LOAD", script);
+}
+
 export function rateLimiter() {
   return async function (req, res, next) {
     const key = `rate:${req.ip}:${req.path}`;
     const now = Date.now();
-    const window = 10000;
-    const limit = 3;
-    await redis.zremrangebyscore(key, 0, now - window);
-    const count = await redis.zcard(key);
-    if (count >= limit) {
+    const result = await redis.evalsha(sha, 1, key, now, 10000, 3, `${now}`);
+    if (result[0] === 0) {
       return res.status(429).send("Too many requests");
     }
-    await redis.zadd(key, now, `${now}`);
-    await redis.expire(key, 10);
     next();
   };
 }

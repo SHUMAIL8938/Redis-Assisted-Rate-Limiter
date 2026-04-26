07f1d4e3b3fb746059a4da8e4d215038d09d544b
SHUMAIL AHMAD SAMAR <shumailahmad8938@gmail.com>
2026-04-26 22:47:33 +0530

    Added Routes

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ async function start() {
   await initRateLimiter();
   app.use(rateLimiter());
   app.get("/", (req, res) => res.send("Hello World"));
+  app.get("/test", (req, res) => res.send("req successful"));
+  app.get("/login", (req, res) => res.send("login successful"));
+  app.get("/debug/:route", async (req, res) => {
+    const key = `rl:${req.ip}:${req.params.route}`;
+    const data = await redis.zrange(key, 0, -1, "WITHSCORES");
+    res.json({ key, entries: data });
+  });
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });

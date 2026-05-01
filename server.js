d639fba38e5d3c09d37f8b92c99fb7f2f1bd38b9
SHUMAIL AHMAD SAMAR <shumailahmad8938@gmail.com>
2026-05-01 20:14:22 +0530

    fixed server routes

--- server.js
+++ server.js
@@ -7,15 +7,22 @@ const PORT = process.env.PORT || 3000;
 app.set("trust proxy", true);
 app.use(express.static("public"));
 async function startServer() {
-  try{
+  try {
     await initRateLimiter();
     app.use(rateLimiter());
-    app.get("/test",async (req, res) => {
-        await new Promise((r)=>setTimeout(r,Math.random()*200));
-        res.set("X-Server-Time",Date.now().toString());
-        res.send("req successful");
+    app.get("/", (req, res) => {
+      res.send("Hello Fellas!");
     });
-    app.get("/login", (req, res) => res.send("login successful"));
+    app.get("/test", async (req, res) => {
+      await new Promise((r) => setTimeout(r, Math.random() * 200));
+      res.set("X-Server-Time", Date.now().toString());
+      res.send("req successful");
+    });
+    app.get("/login", async (req, res) => {
+      await new Promise((r) => setTimeout(r, Math.random() * 200));
+      res.set("X-Server-Time", Date.now().toString());
+      res.send("login successful");
+    });
     app.get("/debug/:route", async (req, res) => {
       const key = `rl:${req.ip}:${req.params.route}`;
       const data = await redis.zrange(key, 0, -1, "WITHSCORES");
@@ -25,9 +32,8 @@ async function startServer() {
       res.json({ status: "ok", time: Date.now() });
     });
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  }
-  catch(err){
-    console.error("Failed to start server:",err);
+  } catch (err) {
+    console.error("Failed to start server:", err);
     process.exit(1);
   }
 }
